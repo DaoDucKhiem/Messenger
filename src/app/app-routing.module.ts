827b3f497cf15025cdbe51d1563b5100e7c720a9
DaoDucKhiem <daoduckhiem1104@gmail.com>
2020-06-29 01:31:31 +0700

    add route message view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MessagesComponent } from './ui/chat/messages/messages.component';
 import { from } from 'rxjs';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/message', pathMatch: 'full' },
+  { path: 'message', component: MessagesComponent }
 ];
 
 @NgModule({
